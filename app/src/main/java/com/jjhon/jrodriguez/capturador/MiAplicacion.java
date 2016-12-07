5eef5578e7d51659fae40d90e03f88110cada5b1
jaimea <jajhonrod@gmail.com>
2016-12-06 19:40:58 -0500

    Boton de logueo

--- app/src/main/java/com/jjhon/jrodriguez/capturador/MiAplicacion.java
+++ app/src/main/java/com/jjhon/jrodriguez/capturador/MiAplicacion.java
@@ -6,9 +6,9 @@ import android.app.Application;
  * Created by jaimea on 12/5/16.
  */
 public class MiAplicacion extends Application{
-    //private String miURL= "http://192.168.0.7/";
-    private String miURL= "http://192.168.2.132:4568/agendamascotas/";
-    //private String miURL= "http://172.17.2.51/";
+    private String miURL= "http://192.168.0.7/agendamascotas/";
+    //private String miURL= "http://192.168.2.132:4568/agendamascotas/";
+    //private String miURL= "http://172.17.2.51/agendamascotas/";
     private String carpetaImage = "imagenes/";
 
     public String getMiURL() {
